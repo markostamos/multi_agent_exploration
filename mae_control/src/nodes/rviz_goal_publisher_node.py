b1d979c0e3b22c2ec940d86375134dbe8441289a
markostamos <gimarkostamos@gmail.com>
2022-01-13 01:32:37 +0200

    added octomap

--- mae_control/src/nodes/rviz_goal_publisher_node.py
+++ mae_control/src/nodes/rviz_goal_publisher_node.py
@@ -2,19 +2,23 @@
 
 
 import rospy
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, Quaternion
+
 
 class rviz_goal_publisher_node:
     def __init__(self):
         rospy.init_node("rviz_goal_publisher_node")
         rospy.loginfo("Starting waypoint_node as name_node.")
         ns = rospy.get_namespace()
-        self.lee_publisher = rospy.Publisher(f'/{ns}/command/pose', PoseStamped, queue_size=10)
+        self.lee_publisher = rospy.Publisher(
+            f'/{ns}/command/pose', PoseStamped, queue_size=10)
         rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.callback)
 
     def callback(self, goal_pose):
         goal_pose.pose.position.z = rospy.get_param('~altitude', default=1)
+        goal_pose.pose.orientation = Quaternion()
         self.lee_publisher.publish(goal_pose)
+
 
 if __name__ == "__main__":
     name_node = rviz_goal_publisher_node()
