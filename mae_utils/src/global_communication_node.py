fb7c60d074607b7b8ab9b77b771963957036c13b
markostamos <gimarkostamos@gmail.com>
2022-10-23 19:16:44 +0300

    global comm refactor

--- mae_utils/src/global_communication_node.py
+++ mae_utils/src/global_communication_node.py
@@ -12,14 +12,17 @@ from std_msgs.msg import String
 
 class global_communication_node:
     """
-    This node subscribes to the lidar sensors of all the uavs and
-    republishes the point clouds to a single topic so that a global
-    map can be made.
+    Simulates global communication for all the agent.
+    Location of all agents is known.
+    Lidar data is published to a single instance of an octomap_server.
+    Dynamically checks for new agent topics based on the agent_handle.
+    Interfaces with the global planner to generate plans for agents that are on Exploration Mode.
     """
 
     def __init__(self):
         rospy.init_node("global_communication_node")
-
+        # Agent topic handle
+        self.agent_handle = rospy.get_param("~agent_handle", "drone")
         # initialize variables
         self.agent_locations = dict()
         self.home_locations = dict()
@@ -38,44 +41,40 @@ class global_communication_node:
         self.plan_publishers = dict()
         self.plan_viz_publishers = dict()
 
-        self.checkpoint_publisher = rospy.Publisher("/checkpoints", PointArray, queue_size=1000)
-
         # initialize services
         rospy.wait_for_service("global_planner_node/make_global_plan")
         self.global_plan_service = rospy.ServiceProxy(
             "global_planner_node/make_global_plan", GlobalPlanService, True)
 
-        rospy.Timer(rospy.Duration(0.5), self.checkForDrones)
+        rospy.Timer(rospy.Duration(0.5), self.checkForAgentTopics)
         rospy.Timer(rospy.Duration(0.1), self.postPCL)
         rospy.Timer(rospy.Duration(2), self.updatePlan)
-        rospy.Timer(rospy.Duration(2), self.publishCheckpoints)
 
-    def checkForDrones(self, event):
+    def checkForAgentTopics(self, event):
         for topic in rospy.get_published_topics():
-            str_loc = topic[0].find("drone")
+            str_loc = topic[0].find(self.agent_handle)
             if str_loc != -1:
-                drone_id = int(topic[0][str_loc + len("drone")])
-                if drone_id not in self.agent_locations.keys():
-                    """ self.agent_locations["home" + str(drone_id)] = rospy.wait_for_message(
-                        f"/drone{drone_id}/ground_truth/position", PointStamped).point """
+                agent_id = int(topic[0][str_loc + len(self.agent_handle)])
+                agent_namespace = self.agent_handle + str(agent_id)
+                if agent_id not in self.agent_locations.keys():
                     rospy.Subscriber(
-                        f"/drone{drone_id}/ground_truth/position", PointStamped, self.updateLocations, drone_id)
+                        f"/{agent_namespace}/ground_truth/position", PointStamped, self.updateLocations, agent_id)
 
-                    rospy.Subscriber(f"/drone{drone_id}/velodyne_points", PointCloud2,
-                                     self.updatePCL, drone_id)
+                    rospy.Subscriber(f"/{agent_namespace}/velodyne_points", PointCloud2,
+                                     self.updatePCL, agent_id)
 
-                    self.plan_publishers[drone_id] = rospy.Publisher(
-                        f"/drone{drone_id}/plan", PointArray, queue_size=1000)
+                    self.plan_publishers[agent_id] = rospy.Publisher(
+                        f"/{agent_namespace}/plan", PointArray, queue_size=1000)
 
-                    self.plan_viz_publishers[drone_id] = rospy.Publisher(
-                        f"/drone{drone_id}/plan_viz", Marker, queue_size=1000)
+                    self.plan_viz_publishers[agent_id] = rospy.Publisher(
+                        f"/{agent_namespace}/plan_viz", Marker, queue_size=1000)
 
-                    rospy.Subscriber(f"/drone{drone_id}/active_task",
-                                     String, self.getActiveTask, drone_id)
+                    rospy.Subscriber(f"/{agent_namespace}/active_task",
+                                     String, self.getActiveTask, agent_id)
 
-                    self.active_tasks[drone_id] = "Idle"
-                    self.home_locations[drone_id] = rospy.wait_for_message(
-                        f"/drone{drone_id}/ground_truth/position", PointStamped).point
+                    self.active_tasks[agent_id] = "Idle"
+                    self.home_locations[agent_id] = rospy.wait_for_message(
+                        f"/{agent_namespace}/ground_truth/position", PointStamped).point
 
     def updateFrontiers(self, msg):
         def significant_change(): return any(
@@ -92,14 +91,13 @@ class global_communication_node:
             self.frontiers = msg
 
     def updatePlan(self, event=None):
-        rospy.logerr("Updating Plan")
-        available_agents = [drone_id for drone_id in list(self.agent_locations.keys())
-                            if self.active_tasks[drone_id] == "Exploration"]
+        available_agents = [agent_id for agent_id in list(self.agent_locations.keys())
+                            if self.active_tasks[agent_id] == "Exploration"]
         targets = self.frontiers.points
         if len(targets) > 0 and len(available_agents) > 0:
             request = GlobalPlanServiceRequest()
             request.starting_positions = PointArray(
-                [self.agent_locations[drone_id] for drone_id in available_agents])
+                [self.agent_locations[agent_id] for agent_id in available_agents])
             request.targets = PointArray(targets)
             request.timeout_ms = len(available_agents) * (50 if len(targets) < 100 else 100)
             response = self.global_plan_service(request)
@@ -107,18 +105,21 @@ class global_communication_node:
                 self.plan_publishers[available_agents[i]].publish(
                     response.global_plan[i])
                 self.publish_plan_visualization(response.global_plan[i], available_agents[i])
+        else:
+            for agent_id in list(self.agent_locations.keys()):
+                self.publish_plan_visualization(PointArray(), agent_id)
 
-    def updatePCL(self, pcl, drone_id):
-        self.drone_pcl[drone_id] = pcl
+    def updatePCL(self, pcl, agent_id):
+        self.drone_pcl[agent_id] = pcl
 
     def postPCL(self, event):
         keys = list(self.drone_pcl.keys())
-        for drone_id in keys:
-            self.pcl_publisher.publish(self.drone_pcl[drone_id])
+        for agent_id in keys:
+            self.pcl_publisher.publish(self.drone_pcl[agent_id])
             rospy.sleep(0.1)
 
-    def updateLocations(self, msg, drone_id):
-        self.agent_locations[drone_id] = msg.point
+    def updateLocations(self, msg, agent_id):
+        self.agent_locations[agent_id] = msg.point
 
     def dist(self, point1, point2):
         return ((point1.x - point2.x)**2 + (point1.y - point2.y)**2 + (point1.z - point2.z)**2)**0.5
@@ -137,9 +138,9 @@ class global_communication_node:
 
         self.map_publisher.publish(map_msg)
 
-    def publish_plan_visualization(self, plan_array, drone_id):
+    def publish_plan_visualization(self, plan_array, agent_id):
         marker = Marker()
-        color = ((1, 0, 0), (0, 0, 1), (1, 0, 1), (0, 1, 0), (1, 1, 0))[(drone_id - 1) % 4]
+        color = ((1, 0, 0), (0, 0, 1), (1, 0, 1), (0, 1, 0), (1, 1, 0))[(agent_id - 1) % 4]
         marker.header.frame_id = "world"
         marker.header.stamp = rospy.Time.now()
         marker.type = Marker.LINE_STRIP
@@ -150,19 +151,14 @@ class global_communication_node:
         marker.color.g = color[1]
         marker.color.b = color[2]
         marker.points = plan_array.points
-        self.plan_viz_publishers[drone_id].publish(marker)
+        self.plan_viz_publishers[agent_id].publish(marker)
 
-    def getActiveTask(self, msg, drone_id):
-        if self.active_tasks[drone_id] != msg.data and msg.data == "Exploration":
-            self.active_tasks[drone_id] = msg.data
+    def getActiveTask(self, msg, agent_id):
+        if self.active_tasks[agent_id] != msg.data and msg.data == "Exploration":
+            self.active_tasks[agent_id] = msg.data
             self.updatePlan()
         else:
-            self.active_tasks[drone_id] = msg.data
-
-    def publishCheckpoints(self, event):
-        msg = PointArray()
-        msg.points = list(self.agent_locations.values())
-        self.checkpoint_publisher.publish(msg)
+            self.active_tasks[agent_id] = msg.data
 
 
 if __name__ == "__main__":
